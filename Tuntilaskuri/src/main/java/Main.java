0c34a37e3dceb75937b61e861307df4109d415c1
Samuli Pirttimaki <samuli.pirttimaki@helsinki.fi>
2018-11-13 21:07:53 +0200

    lisätty UIta

--- Tuntilaskuri/src/main/java/Main.java
+++ Tuntilaskuri/src/main/java/Main.java
@@ -1,9 +1,10 @@
+
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Samuli
@@ -15,8 +16,8 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic 
-        
+
         System.out.println("Hello world!");
     }
-    
+
 }

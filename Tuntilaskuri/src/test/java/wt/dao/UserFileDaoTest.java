9321d92887d975bdaeccc51c659365df91f06e13
Samuli Pirttimaki <samuli.pirttimaki@helsinki.fi>
2018-11-27 22:15:58 +0200

    lisätty testejä kattamaan vaatimukset

--- Tuntilaskuri/src/test/java/wt/dao/UserFileDaoTest.java
+++ Tuntilaskuri/src/test/java/wt/dao/UserFileDaoTest.java
@@ -9,6 +9,7 @@ import java.io.File;
 import java.io.FileWriter;
 import static org.junit.Assert.assertEquals;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 import wt.domain.User;
@@ -18,33 +19,39 @@ import wt.domain.User;
  * @author Samuli
  */
 public class UserFileDaoTest {
-    
-    TemporaryFolder testFolder = new TemporaryFolder();
-    
+
+    @Rule
+    public TemporaryFolder testFolder = new TemporaryFolder();
+
     File userFile;
     UserDao dao;
-    
+
     @Before
     public void setUp() throws Exception {
-        userFile = testFolder.newFile("testfile_users.txt");  
-        
-        try (FileWriter file = new FileWriter(userFile.getAbsolutePath())) {
-            file.write("testertester;Teppo Testaaja\n");
+        userFile = testFolder.newFile("testfile_users.txt");
+
+        try (FileWriter fw = new FileWriter(userFile.getAbsolutePath())) {
+            fw.write("Samuli;Pirttimaki;SPir\n");
         }
-        
+
         dao = new UserFileDao(userFile.getAbsolutePath());
     }
-    
+
     @Test
-    public void UsersAddedToListCorrectly() throws Exception {
-              
+    public void newUserIsSaved() throws Exception {
+        User u = new User("Matti", "Meika");
+        dao.createUser(u);
+
+        User listUser = dao.findByUsername("MMei");
+        assertEquals("Matti", listUser.getFirstname());
+        assertEquals("Meika", listUser.getSurname());
     }
-    
+
     @Test
-    public void NonExistingUserFound() {
+    public void nonExistingUserFound() {
         User u = dao.findByUsername("matti");
-        
+
         assertEquals(null, u);
     }
-    
+
 }
